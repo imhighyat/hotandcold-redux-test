0db03c266ea31849a35283cc01826a653cf1dd04
Hayat Mazzarella <imhighyat@gmail.com>
2018-02-24 23:38:47 -0800

    added test for guess-count

---
+++ src/components/guess-count.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { GuessCount } from './guess-count';
+
+describe('<GuessCount />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessCount />);
+  });
+
+  it('Renders the correct guess count', () => {
+    const count = 5;
+    const wrapper = shallow(<GuessCount guessCount={count} />);
+    expect(wrapper.text()).toEqual(`You've made ${count} guesses!`);
+  });
+});
